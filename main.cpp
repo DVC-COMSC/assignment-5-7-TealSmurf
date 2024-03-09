30c3624c41813ae6b6676559d6fea4047d8cf356
TealSmurf <jcoin707@gmail.com>
2024-03-08 19:44:44 -0800

    Created program that accepts two values, and the number of sequences the user wants which includes the first two values. The program then prints the first two values. Then it adds the two previous dispalyed values and prints them continuously until the number of sequences is met.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,17 @@ int main()
 	int n1, n2;
 	int N;
 
+	cout << "Enter the two initial values: ";
 	cin >> n1 >> n2;
+	cout << "Enter the number of sequences: ";
 	cin >> N;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+	cout << n1 << ", " << n2;
+	for(int i = 0; i + 2 < N; i++)
+	{
+		int n3 = n1 + n2;
+		cout << ", " << n3;
+		n1 = n2;
+		n2 = n3;
+	}
 }
